bea1213e78da751ba8cdef8b890ec7f4898c9125
mizuryu7 <timeo.caillard@gmail.com>
2026-03-27 17:22:07 +0100

    commit

--- login.php
+++ login.php
@@ -7,6 +7,14 @@
   <script src="script.js"></script>
 </head>
 <body>
+  <h1>Login</h1>
+  <form action="login.php" method="post">
+    <label for="username">Nom:</label>
+    <input type="text" id="username" name="username" required><br><br>
+    <label for="password">Mot de passe:</label>
+    <input type="password" id="password" name="password" required><br><br>
+    <input type="submit" value="Login">
+  </form>
   
 </body>
 </html>
